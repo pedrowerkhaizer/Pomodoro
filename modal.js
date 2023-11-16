4540a161e948b47a1c91390242f2e88c6ef0feb7
Pedro Werkhaizer <pedro.werkhaizer@gmail.com>
2023-11-15 21:18:33 -0300

    Delete modal.js

--- modal.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () =>{
-let modal = document.getElementById("modal");
-let closeModal = document.getElementById("close-modal");
-
-modal.style.display = "none";
-
-
-})
